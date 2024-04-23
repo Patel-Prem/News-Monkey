450bcafd25faa33f53abedb96665bd2d7bfcae6f
Patel-Prem <patelprem12320@gmail.com>
2024-04-22 22:10:32 -0600

    added prvisous and next button in News Component and use better approch to set values of state.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 import '../Assets/Style/Navbar.css'
 
 export class Navbar extends Component {
@@ -7,91 +8,30 @@ export class Navbar extends Component {
             <div className='sticky'>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top" id='navbar'>
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">NewsMonkey</a>
+                        <Link className="navbar-brand" to="/">NewsMonkey</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                    <Link className="nav-link active" aria-current="page" href="/">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
+                                    <Link className="nav-link" href="/about">About</Link>
                                 </li>
                                 <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" href="/" id="categoryDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <Link className="nav-link dropdown-toggle" href="/" id="categoryDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         Category
-                                    </a>
+                                    </Link>
                                     <ul className="dropdown-menu" aria-labelledby="categoryDropdown" style={{height: 'calc( 5 * 100% )', overflow:'auto'}}>
-                                        <li><a className="dropdown-item" value="business" href="/">Business</a></li>
-                                        <li><a className="dropdown-item" value="entertainment" href="/">Entertainment</a></li>
-                                        <li><a className="dropdown-item" value="general" href="/">General</a></li>
-                                        <li><a className="dropdown-item" value="health" href="/">Health</a></li>
-                                        <li><a className="dropdown-item" value="science" href="/">Science</a></li>
-                                        <li><a className="dropdown-item" value="sports" href="/">Sports</a></li>
-                                        <li><a className="dropdown-item" value="technology" href="/">Technology</a></li>
-                                    </ul>
-                                </li>
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" href="/" id="countryDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Country
-                                    </a>
-                                    <ul className="dropdown-menu" aria-labelledby="countryDropdown" style={{height: 'calc( 5 * 100% )', overflow:'auto'}}>
-                                        <li><a className="dropdown-item" value="ae" href="/">ae</a></li>
-                                        <li><a className="dropdown-item" value="ar" href="/">ar</a></li>
-                                        <li><a className="dropdown-item" value="at" href="/">at</a></li>
-                                        <li><a className="dropdown-item" value="au" href="/">au</a></li>
-                                        <li><a className="dropdown-item" value="be" href="/">be</a></li>
-                                        <li><a className="dropdown-item" value="bg" href="/">bg</a></li>
-                                        <li><a className="dropdown-item" value="br" href="/">br</a></li>
-                                        <li><a className="dropdown-item" value="ca" href="/">ca</a></li>
-                                        <li><a className="dropdown-item" value="ch" href="/">ch</a></li>
-                                        <li><a className="dropdown-item" value="cn" href="/">cn</a></li>
-                                        <li><a className="dropdown-item" value="co" href="/">co</a></li>
-                                        <li><a className="dropdown-item" value="cu" href="/">cu</a></li>
-                                        <li><a className="dropdown-item" value="cz" href="/">cz</a></li>
-                                        <li><a className="dropdown-item" value="de" href="/">de</a></li>
-                                        <li><a className="dropdown-item" value="eg" href="/">eg</a></li>
-                                        <li><a className="dropdown-item" value="fr" href="/">fr</a></li>
-                                        <li><a className="dropdown-item" value="gb" href="/">gb</a></li>
-                                        <li><a className="dropdown-item" value="gr" href="/">gr</a></li>
-                                        <li><a className="dropdown-item" value="hk" href="/">hk</a></li>
-                                        <li><a className="dropdown-item" value="hu" href="/">hu</a></li>
-                                        <li><a className="dropdown-item" value="id" href="/">id</a></li>
-                                        <li><a className="dropdown-item" value="ie" href="/">ie</a></li>
-                                        <li><a className="dropdown-item" value="il" href="/">il</a></li>
-                                        <li><a className="dropdown-item" value="in" href="/">in</a></li>
-                                        <li><a className="dropdown-item" value="it" href="/">it</a></li>
-                                        <li><a className="dropdown-item" value="jp" href="/">jp</a></li>
-                                        <li><a className="dropdown-item" value="kr" href="/">kr</a></li>
-                                        <li><a className="dropdown-item" value="lt" href="/">lt</a></li>
-                                        <li><a className="dropdown-item" value="lv" href="/">lv</a></li>
-                                        <li><a className="dropdown-item" value="ma" href="/">ma</a></li>
-                                        <li><a className="dropdown-item" value="mx" href="/">mx</a></li>
-                                        <li><a className="dropdown-item" value="my" href="/">my</a></li>
-                                        <li><a className="dropdown-item" value="ng" href="/">ng</a></li>
-                                        <li><a className="dropdown-item" value="nl" href="/">nl</a></li>
-                                        <li><a className="dropdown-item" value="no" href="/">no</a></li>
-                                        <li><a className="dropdown-item" value="nz" href="/">nz</a></li>
-                                        <li><a className="dropdown-item" value="ph" href="/">ph</a></li>
-                                        <li><a className="dropdown-item" value="pl" href="/">pl</a></li>
-                                        <li><a className="dropdown-item" value="pt" href="/">pt</a></li>
-                                        <li><a className="dropdown-item" value="ro" href="/">ro</a></li>
-                                        <li><a className="dropdown-item" value="rs" href="/">rs</a></li>
-                                        <li><a className="dropdown-item" value="ru" href="/">ru</a></li>
-                                        <li><a className="dropdown-item" value="sa" href="/">sa</a></li>
-                                        <li><a className="dropdown-item" value="se" href="/">se</a></li>
-                                        <li><a className="dropdown-item" value="sg" href="/">sg</a></li>
-                                        <li><a className="dropdown-item" value="si" href="/">si</a></li>
-                                        <li><a className="dropdown-item" value="sk" href="/">sk</a></li>
-                                        <li><a className="dropdown-item" value="th" href="/">th</a></li>
-                                        <li><a className="dropdown-item" value="tr" href="/">tr</a></li>
-                                        <li><a className="dropdown-item" value="tw" href="/">tw</a></li>
-                                        <li><a className="dropdown-item" value="ua" href="/">ua</a></li>
-                                        <li><a className="dropdown-item" value="us" href="/">us</a></li>
-                                        <li><a className="dropdown-item" value="ve" href="/">ve</a></li>
-                                        <li><a className="dropdown-item" value="za" href="/">za</a></li>
+                                        <li><Link className="dropdown-item" value="business" to="/business">Business</Link></li>
+                                        <li><Link className="dropdown-item" value="entertainment" to="/entertainment">Entertainment</Link></li>
+                                        <li><Link className="dropdown-item" value="general" to="/general">General</Link></li>
+                                        <li><Link className="dropdown-item" value="health" to="/health">Health</Link></li>
+                                        <li><Link className="dropdown-item" value="science" to="/science">Science</Link></li>
+                                        <li><Link className="dropdown-item" value="sports" to="/sports">Sports</Link></li>
+                                        <li><Link className="dropdown-item" value="technology" to="/technology">Technology</Link></li>
                                     </ul>
                                 </li>
                             </ul>
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react'
-// import Navbar from './Navbar'
-import NewsItem from './NewsItem'
-// import Spinner from './Spinner'
-// import ReactPaginate from 'react-paginate';
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import PropTypes from "prop-types";
 
 export class News extends Component {
-
   static defaultProps = {
-    country: 'in',
-    category: 'general',
-    pageSize: 12
-
-  }
+    country: "in",
+    category: "general",
+    pageSize: 12,
+  };
 
   static propsType = {
     country: PropTypes.string,
     category: PropTypes.string,
-    pageSize: PropTypes.number
-  }
-
+    pageSize: PropTypes.number,
+  };
 
   constructor() {
     super();
@@ -27,55 +21,65 @@ export class News extends Component {
     this.state = {
       articles: [],
       isLoading: false,
-      totalRowCount: 0,
-      perPage: 12,
-      pageCount: 0,
-    }
+      totalResults: 0,
+      // pageSize: this.props.pageSize,
+      pages: 0,
+      pageCount: 1,
+    };
   }
 
   async componentDidMount() {
-    this.setState({ isLoading: true });
+    // this.setState({ isLoading: true });
+    // console.log("componentDidMount");
 
-    // let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=bcaa0c7987b0437db17fa96548c83a76&pageSize=12`
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bcaa0c7987b0437db17fa96548c83a76&pageSize=${this.props.pageSize}`
+    // let url = `https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=bcaa0c7987b0437db17fa96548c83a76&pageSize=12&page=1`
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bcaa0c7987b0437db17fa96548c83a76&pageSize=${this.props.pageSize}&page=${this.state.pageCount}`;
+    console.log(url)
     await fetch(url)
       .then((response) => response.json())
       .then((response) => {
-        console.log('response : ', response)
-        this.setState({
-          totalRowCount: response.totalResults,
-          articles: response.articles,
-          isLoading: false
-        })
+        console.log("response : ", response);
+        this.setState(
+          (prevState) => ({
+            ...prevState,
+            articles: response.articles,
+            isLoading: false,
+            totalResults: response.totalResults,
+          }),
+          () => {
+            this.setState((prevState) => ({
+              ...prevState,
+              pages: Math.ceil(this.state.totalResults / this.props.pageSize),
+            }));
+          }
+        );
       });
-
-    this.setState({
-      pageCount: Math.ceil(this.state.totalRowCount / this.state.perPage),
-    })
   }
 
-  // handlePageChange = async (data) => {
+  previousBtn = async () => {
+    // console.log("Previous")
+    this.setState({
+      articles: this.state.articles,
+      isLoading: this.state.isLoading,
+      totalResults: this.state.totalResults,
+      pages: this.state.pages,
+      pageCount: this.state.pageCount - 1,
+    });
+  };
 
-  //   this.setState({ isLoading: true });
-
-  //   console.log('pagechnage', data.selected + 1)
-
-  //   let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=bcaa0c7987b0437db17fa96548c83a76&pageSize=12&page=${data.selected + 1}`;
-
-  //   fetch(url)
-  //   .then((response) => response.json())
-  //   .then((response) => {
-  //     console.log('response : ', response)
-  //     this.setState({
-  //       totalRowCount: response.totalResults,
-  //       articles: response.articles,
-  //       isLoading: false
-  //     })
-  //   });
-  // }
+  nextBtn = async () => {
+    console.log("Next");
+    this.setState({
+      articles: this.state.articles,
+      isLoading: this.state.isLoading,
+      totalResults: this.state.totalResults,
+      pages: this.state.pages,
+      pageCount: this.state.pageCount + 1,
+    });
+  };
 
   render() {
-    console.log('render')
+    // console.log('render')
     return (
       <div>
         {/* { 
@@ -83,72 +87,54 @@ export class News extends Component {
           <Spinner /> 
         } */}
 
-        {
-          !this.state.isLoading &&
+        {!this.state.isLoading && (
           <div className="container-fluid my-3">
             <h3>Welcome to NewsMokey's top headlined</h3>
             <hr />
-            {/* <ReactPaginate
-              pageCount={this.state.pageCount}
-              pageRangeDisplayed={2}
-              marginPagesDisplayed={2}
-              previousLabel={'Previous'}
-              nextLabel={'Next'}
-              breakLabel={'. . .'}
-              containerClassName={'pagination justify-content-end m-3'}
-              pageClassName={'page-item'}
-              pageLinkClassName={'page-link'}
-              previousClassName={'page-item'}
-              previousLinkClassName={'page-link'}
-              nextClassName={'page-item'}
-              nextLinkClassName={'page-link'}
-              breakClassName={'page-item'}
-              breakLinkClassName={'page-link'}
-              activeClassName={'active'}
-              activeLinkClassName={'page-link'}
-              onPageChange={this.handlePageChange}
-            /> */}
+
+            {/* <div className='container-fluide d-flex justify-content-between'>
+              <button type="button" class="btn btn-primary">&larr; Privious</button>
+              <button type="button" class="btn btn-primary">Next &rarr;</button>
+            </div> */}
 
             <div className="d-flex flex-wrap justify-content-center align-items-center">
               {this.state.articles.map((element) => {
-                return <div key={element.url}>
-                  <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
-                </div>
+                return (
+                  <div key={element.url}>
+                    <NewsItem
+                      title={element.title}
+                      description={element.description}
+                      imageUrl={element.urlToImage}
+                      newsUrl={element.url}
+                    />
+                  </div>
+                );
               })}
             </div>
 
-            {/* <div className="row">
-              {this.state.articles.map((element) => {
-                return <div key={element.url} className="col-md-4">
-                  <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
-                </div>
-              })}
-            </div> */}
-
-            {/* <ReactPaginate
-              pageCount={this.state.pageCount}
-              pageRangeDisplayed={2}
-              marginPagesDisplayed={2}
-              previousLabel={'Previous'}
-              nextLabel={'Next'}
-              breakLabel={'. . .'}
-              containerClassName={'pagination justify-content-end m-3'}
-              pageClassName={'page-item'}
-              pageLinkClassName={'page-link'}
-              previousClassName={'page-item'}
-              previousLinkClassName={'page-link'}
-              nextClassName={'page-item'}
-              nextLinkClassName={'page-link'}
-              breakClassName={'page-item'}
-              breakLinkClassName={'page-link'}
-              activeClassName={'active'}
-              activeLinkClassName={'page-link'}
-              onPageChange={this.handlePageChange}
-            /> */}
+            <div className="container d-flex justify-content-between">
+              <button
+                disabled={this.state.pageCount <= 1}
+                type="button"
+                className="btn btn-primary"
+                onClick={this.previousBtn}
+              >
+                &larr; Previous
+              </button>
+              {/* <button type="button" className="btn btn-primary" onClick={this.nextBtn}>Next &rarr;</button> */}
+              <button
+                disabled={this.state.pageCount >= this.state.pages}
+                type="button"
+                className="btn btn-primary"
+                onClick={this.nextBtn}
+              >
+                Next &rarr;
+              </button>
+            </div>
           </div>
-        }
+        )}
       </div>
-    )
+    );
   }
 }
-export default News
+export default News;
